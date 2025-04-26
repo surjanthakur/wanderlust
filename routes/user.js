338e2b4292a53a4b19634c95eee086e2b3defb9d
surjan thakur <tsurjan506@gmail.com>
2025-04-26 10:42:23 +0530

    save

--- routes/user.js
+++ routes/user.js
@@ -11,10 +11,15 @@ router.get("/signup", (req, res) => {
 
 router.post(
   "/signup",
-  wrapAsync(async (req, res) => {
+  wrapAsync(async (req, res, next) => {
     let { username, email, password } = req.body;
     let newUser = new User({ username, email });
     const registerUser = await User.register(newUser, password);
+    req.login(registerUser, (err) => {
+      if (err) {
+        return next(err)
+      }
+    });
     console.log(registerUser);
     req.flash("success", "welcome to wanderlust");
     res.redirect("/listings");
@@ -38,5 +43,16 @@ router.post(
     res.redirect("/listings");
   })
 );
+
+//logout route 
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You have successfully logged out!");
+    res.redirect("/listings");
+  });
+});
 
 module.exports = router;
